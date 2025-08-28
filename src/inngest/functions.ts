7ef731a35ecce60ea18133946bbc12b90387c9fa
adparekh <aparekh0110@gmail.com>
2025-08-28 19:02:06 -0400

    05: AI jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+export const summarizeContents = inngest.createFunction(
+  { id: "summarize-contents" },
+  { event: "app/ticket.created" },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
